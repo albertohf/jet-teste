33d63fd76a86c34fc5f5eef28dd4e5dfbddf002c
Albertohf <albertohf91@gmail.com>
2024-03-05 23:12:23 -0300

    message TG-5 #inprogress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,6 @@ export default function Home() {
             </BrowserView>
             <MobileView>
               <TableSkeletonMobile />
-              <TableSkeletonMobile />
             </MobileView>
           </>
         )}
